9015efbb448744f91533ff4f792eb62788e36ea8
nikurou <47164155+nikurou@users.noreply.github.com>
2021-10-26 00:29:01 -1000

    Color constants to be used

---
+++ src/constants/Color.js
@@ -0,0 +1,38 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  /* Site Colors*/
+  primaryColor: "#ffffff",
+  accentColor: "#ff6f00",
+  blackText: "#131313",
+  redText: "#e71316",
+  backgroundPrimary: "rgb(15,15,15)",
+  backgroundSecondary: "#e71316",
+  backgroundColorLight: "",
+  headerTitle: "#e71316",
+  headerContent: "#e5e5e5",
+
+  /* Light Mode */
+
+  /* Dark Mode */
+  backgroundColorDark: "#0a1929",
+
+  /* Type Colors*/
+  normalType: "",
+  fightingType: "",
+  flyingType: "",
+  poisonType: "",
+  groundType: "",
+  rockType: "",
+  bugType: "",
+  ghostType: "",
+  steelType: "",
+  fireType: "",
+  waterType: "",
+  grassType: "",
+  electricType: "",
+  psychicType: "",
+  iceType: "",
+  dragonType: "",
+  darkType: "",
+  fairyType: "",
+};
