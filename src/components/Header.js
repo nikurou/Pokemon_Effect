2f8440cae1051d9a4bf257c5ae30653a92f80d58
nikurou <47164155+nikurou@users.noreply.github.com>
2021-10-26 22:18:38 -1000

    Button Active/Inactive Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Color from "../constants/Color";
-import { Button } from "@mui/material";
+import { Button } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 
 /* Serve as a Title + NavBar 
@@ -33,14 +33,41 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "center",
   },
+  button: {
+    color: Color.blackText,
+    transition: "0.5s",
+    "& :hover , &$focusVisible": {
+      color: Color.redText,
+      fontWeight: 550,
+      "&:hover $buttonLine": { backgroundColor: Color.redText, width: "100%" },
+    },
+  },
+  activeButton: {
+    color: Color.redText,
+    fontWeight: 550,
+    "& $buttonLine": { backgroundColor: Color.redText, width: "100%" },
+  },
+  buttonLine: {
+    height: 3,
+    width: 30,
+    backgroundColor: "transparent",
+    position: "absolute",
+    bottom: -2,
+    marginRight: "50%",
+    marginLeft: "50%",
+    transition: "0.2s",
+  },
 }));
 
 const Header = (props) => {
   const classes = useStyles();
   let history = useHistory();
+  const [activeTab, setActiveTab] = useState("typeEffect");
 
+  // Route to path page
   function handleClick(path) {
     history.push(`/${path}`);
+    setActiveTab(path);
   }
 
   return (
@@ -48,8 +75,42 @@ const Header = (props) => {
       <div className={classes.headerTitle}>Pkm Effect</div>
       <div className={classes.randomAssBlackLine}></div>
       <div className={classes.headerContent}>
-        <Button onClick={() => handleClick("typeEffect")}>Type Effect</Button>
-        <Button onClick={() => handleClick("info")}>Info</Button>
+        <Button
+          className={
+            activeTab === "team" ? classes.activeButton : classes.button
+          }
+          onClick={() => handleClick("team")}
+        >
+          My Team
+          <span className={classes.buttonLine} />
+        </Button>
+        <Button
+          className={
+            activeTab === "typeEffect" ? classes.activeButton : classes.button
+          }
+          onClick={() => handleClick("typeEffect")}
+        >
+          Type Effect
+          <span className={classes.buttonLine} />
+        </Button>
+        <Button
+          className={
+            activeTab === "pokedex" ? classes.activeButton : classes.button
+          }
+          onClick={() => handleClick("pokedex")}
+        >
+          Pokedex
+          <span className={classes.buttonLine} />
+        </Button>
+        <Button
+          className={
+            activeTab === "info" ? classes.activeButton : classes.button
+          }
+          onClick={() => handleClick("info")}
+        >
+          Info
+          <span className={classes.buttonLine} />
+        </Button>
       </div>
     </div>
   );
