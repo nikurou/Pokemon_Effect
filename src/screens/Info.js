815c2cf898b1f136bef042c08be60b03d795d63f
nikurou <47164155+nikurou@users.noreply.github.com>
2021-12-09 00:24:50 -1000

    moved to screens folder

---
+++ src/screens/Info.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Color from "../constants/Color";
+
+const useStyles = makeStyles((theme) => ({}));
+
+const Info = () => {
+  const classes = useStyles();
+  return <div>Information</div>;
+};
+
+export default Info;
