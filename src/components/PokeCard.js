2a946bc333c7028bd9b4df196e8706ce3497d657
nikurou <47164155+nikurou@users.noreply.github.com>
2021-12-19 14:17:54 -1000

    Now displays typing with proper capitilization

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -34,6 +34,22 @@ const PokeCard = (props) => {
   const [id, setID] = useState(0);
   const [types, setTypes] = useState([]);
 
+  // Capitalize first letter of input where input can be a string or an array of strings
+  const toUpperFirstCase = (input) => {
+    var temp = input;
+
+    //Depending on if input is []
+    if (typeof input === "object") {
+      for (var i = 0; i < temp.length; i++) {
+        temp[i] = temp[i].charAt(0).toUpperCase() + temp[i].slice(1);
+      }
+      // if input is string
+    } else if (typeof input === "string") {
+      temp = input.charAt(0).toUpperCase() + input.slice(1);
+    }
+    return temp;
+  };
+
   //Fetch Pokemon Object on Initial Render of Component
   useEffect(() => {
     axios
@@ -42,7 +58,11 @@ const PokeCard = (props) => {
         setName(res.data.name);
         setSprite(res.data.sprites.front_default);
         setID(res.data.id);
-        setTypes(res.data.types);
+        setTypes(res.data.types[0].type.name);
+        //if it's dual typing
+        if (res.data.types[1] != null) {
+          setTypes([res.data.types[0].type.name, res.data.types[1].type.name]);
+        }
       })
       .catch((error) => console.log(error));
   }, []);
@@ -53,9 +73,9 @@ const PokeCard = (props) => {
         <img src={spriteIcon} alt="spriteIcon"></img>
       </div>
       <div className={classes.rightContainer}>
-        <h3>{name}</h3>
+        <h3>{toUpperFirstCase(name)}</h3>
         <p> {`ID: ${id}`}</p>
-        <p> Types: ?</p>
+        <p> {`Types: ${toUpperFirstCase(types)}`}</p>
       </div>
     </div>
   );
