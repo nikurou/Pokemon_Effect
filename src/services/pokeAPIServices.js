1c0d9e93c68494e4d56c288126b23af3af20905a
nikurou <47164155+nikurou@users.noreply.github.com>
2022-02-16 23:19:14 -1000

    Service worker to call PokeAPI

---
+++ src/services/pokeAPIServices.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+const baseUrl = "https://pokeapi.co/api/v2/";
+
+const getTypeDamageRelations = (type) => {
+  const request = axios.get(`${baseUrl}/type/${type}`);
+  return request.then((res) => res.damage_relations);
+};
+
+export default { getTypeDamageRelations };
